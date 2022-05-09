92d039605b44ac94b6ca9209131aa094de99ea4e
Joseph McLaughlin <cewmjcm@gmail.com>
2022-05-09 11:19:16 -0700

    Add convergence tests.

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <tgmath.h>
 #include <iomanip>
+#include <optional>
+#include <iostream>
+#include <cmath>
 
 #include "infrastructure.h" 
 #include "ranges.h"
@@ -18,27 +21,50 @@
 
 #include "poisson.h"
 
+void poisson_1d_convergence_test(
+  std::size_t volume_points, 
+  std::size_t blocks);
+
 int main (int argc, char *argv[]) {
-    
-    if (!infrastructure::initialize()) exit(-1);; { 
 
+  // Initialize petsc, among other libraries.
+  if (!infrastructure::initialize()) exit(-1);; { 
 
-    /* We are solving the Poisson Equation in 1D
+    poisson_1d_convergence_test(81, 5);
 
-        ⎧ u_xx π^2 sin(πx) = 0 , 0 < x < 1
-        ⎨ u = 0    (Dirichlet) , x = 0 
-        ⎩ u_x = -π (Neumann)   , x = 1 
-    */
+  // Cleanup petsc 
+  } infrastructure::cleanup(); return 0;
+}
 
+void poisson_1d_convergence_test(
+  std::size_t volume_points, 
+  std::size_t blocks) {
+
+  /* Solving the Poisson Equation in 1D
+
+    ⎧ u_xx π^2 sin(πx) = 0 , 0 < x < 1
+    ⎨ u = 0    (Dirichlet) , x = 0 
+    ⎩ u_x = -π (Neumann)   , x = 1 
+
+  */
+
+  std::cout << "[ Poisson 1D equation with arbitrary flux " 
+    << "interfaces. ]" << std::endl;
+
+  std::cout << "volume points | error                | " 
+    << "prior/error          | lg2(prior/error) - 2" 
+    << std::endl;
+
+  std::cout << std::setprecision(14) << std::scientific;
+
+  std::optional<long double> prior_error = std::nullopt;
+  for (auto i = 0; i < 8; ++i) {
 
     std::vector<type::real_t> exact;
-    poisson1d::analytical_blocked(exact, 100, 2);
+    poisson1d::analytical_blocked(exact, volume_points, blocks);
 
     std::vector<type::real_t> result;
-    poisson1d::petsc_problem(result, 100, 2);
-
-
-    std::cout << std::setprecision(14);
+    poisson1d::petsc_problem(result, volume_points, blocks);
 
     long double error = 0;
     long double temp;
@@ -46,13 +72,19 @@ int main (int argc, char *argv[]) {
         temp = result[i] - exact[i];
         error += temp * temp;
     }
-   
+ 
     error = std::sqrt(error  / (result.size() / 2)) ;
 
-    std::cout << exact.size() << " : " << error << std::endl;
+    std::cout << std::setw(13) 
+      << exact.size() << " | " << error << " | ";
+    if (prior_error) {
+      std::cout << *prior_error / error << " | " 
+        << std::log2(*prior_error / error) - 2.;
+    }
+    std::cout << std::endl;
 
-    // TODO: Implement convergence tests, normalized with H
-
-    } infrastructure::cleanup(); return 0;
-
+    prior_error = error;
+    volume_points *= 2;
+    volume_points += blocks - 1;
+  }
 }
